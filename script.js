23cdc897b46b579dfd471ecba25bf01bc9fe5c97
Alif Bin Khorshed Ahmed <alifbink@ualberta.ca>
2023-07-20 23:18:00 -0600

    Greeting is functional.

--- script.js
+++
@@ -1,9 +0,0 @@
-
-let greeting = document.querySelector("#greeting");
-
-document.addEventListener("DOMContentLoaded", function () {
-    setTimeout(() => {
-    greeting.classList.remove("fade-in")
-    greeting.classList.add("no-display");
-    }, 3000);
-  });
